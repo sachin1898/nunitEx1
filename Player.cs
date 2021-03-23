f1c1c544d51d7b0d51bc4a35a8c48a5157321d97
sachin <sachinkumars1998@gmail.com>
2021-03-23 11:00:07 +0530

    added both files

---
+++ Player.cs
@@ -0,0 +1,11 @@
+﻿
+namespace ConsoleAppTestExample
+{
+  
+    class Player
+    {
+        public int PlayerId { get; set; }
+        public string PlayerName { get; set; }
+        public string PlayerTeam { get; set; }
+    }
+}
---
+++ PlayerTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+
+namespace ConsoleAppTestExample
+{
+    [TestFixture]
+    public class PlayerTest
+    {
+        List<Player> pList = new List<Player>() { 
+        new Player(){PlayerId=1,PlayerName="virat kohli",PlayerTeam="india"},
+        new Player(){PlayerId=2,PlayerName="joe root",PlayerTeam="england"},
+        new Player(){PlayerId=3,PlayerName="david warner",PlayerTeam="australia"}
+        };
+        [Test]
+        public void CheckNull()
+        {
+            foreach(var p in pList)
+            {
+                Assert.NotZero(p.PlayerId);
+                Assert.NotNull(p.PlayerName);
+                Assert.NotNull(p.PlayerTeam);
+            }
+        }
+
+    }
+}
